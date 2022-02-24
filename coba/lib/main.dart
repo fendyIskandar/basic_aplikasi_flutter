ece2ee7f9e2922cd8848018587b3f829f8828099
fendyIskandar <fendyaremania87@gmail.com>
2022-02-24 09:18:19 -0800

    assets done

--- coba/lib/main.dart
+++ coba/lib/main.dart
@@ -15,97 +15,126 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               title: const Text("Example App"),
             ),
-            body: Container(
-                margin: const EdgeInsets.all(10),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Column(
-                              children: const <Widget>[
-                                Text('BERITA TERBARU'),
-                              ],
-                            ),
-                            Column(
-                              children: const <Widget>[
-                                Text('PERTANDINGAN HARI INI'),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Row(
+            body: ListView(
+              children: [
+                Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Expanded(
-                            child: Image.network(
-                              "https://picsum.photos/seed/picsum/400/200",
-                              fit: BoxFit.fill,
-                            ),
-                          )
-                        ],
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          "Costa Mendekat Ke Palmeiras",
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w900),
-                        ),
-                      ),
-                      Row(children: [
-                        Container(
-                          decoration:
-                              const BoxDecoration(color: Colors.purpleAccent),
-                          margin: const EdgeInsets.only(top: 10),
-                          padding: const EdgeInsets.only(
-                              right: 380, top: 10, bottom: 10),
-                          child: const Text("   Transfer  "),
-                        ),
-                      ]),
-                      Padding(padding: const EdgeInsets.only(top: 10)),
-                      Row(children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              width: 1,
+                          Container(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Column(
+                                  children: const <Widget>[
+                                    Text('BERITA TERBARU'),
+                                  ],
+                                ),
+                                Column(
+                                  children: const <Widget>[
+                                    Text('PERTANDINGAN HARI INI'),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                          child: Row(
+                          Row(
                             children: [
-                              Column(
+                              Expanded(
+                                child: Image.asset(
+                                  "assets/images/gambar.jpg",
+                                  fit: BoxFit.fill,
+                                ),
+                              )
+                            ],
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10),
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Costa Mendekat Ke Palmeiras",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.normal,
+                                  fontWeight: FontWeight.w900),
+                            ),
+                          ),
+                          Row(children: [
+                            Container(
+                              decoration: const BoxDecoration(
+                                  color: Colors.purpleAccent),
+                              margin: const EdgeInsets.only(top: 10),
+                              padding: const EdgeInsets.only(
+                                  right: 380, top: 10, bottom: 10),
+                              child: const Text("   Transfer  "),
+                            ),
+                          ]),
+                          const Padding(padding: EdgeInsets.only(top: 10)),
+                          Row(children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  width: 1,
+                                ),
+                              ),
+                              child: Row(
                                 children: [
-                                  Container(
-                                    height: 150,
-                                    width: 216,
-                                    decoration: const BoxDecoration(
-                                      image: DecorationImage(
-                                        image: NetworkImage(
-                                            "https://picsum.photos/150"),
-                                        fit: BoxFit.fitWidth,
+                                  Column(
+                                    children: [
+                                      Container(
+                                        height: 150,
+                                        width: 216,
+                                        decoration: const BoxDecoration(
+                                          image: DecorationImage(
+                                            image: NetworkImage(
+                                                "https://picsum.photos/150"),
+                                            fit: BoxFit.fitWidth,
+                                          ),
+                                        ),
                                       ),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Column(
+                                      children: const [
+                                        Text("Pique Bilang Wasit Untungkan",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w500,
+                                            )),
+                                        Text("Madrid, Koeman Tepok Jidat",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w500,
+                                            )),
+                                      ],
                                     ),
                                   ),
                                 ],
                               ),
-                              Padding(
-                                padding: const EdgeInsets.all(15.0),
-                                child: Column(
+                            ),
+                          ]),
+                          Row(children: [
+                            Container(
+                              height: 40,
+                              width: 448,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  width: 1,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
                                   children: const [
-                                    Text("Pique Bilang Wasit Untungkan",
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w500,
-                                        )),
-                                    Text("Madrid, Koeman Tepok Jidat",
+                                    Text("Barcelona Feb 13, 2021",
                                         style: TextStyle(
                                           fontSize: 15,
                                           fontWeight: FontWeight.w500,
@@ -113,35 +142,10 @@ class MyApp extends StatelessWidget {
                                   ],
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-                      ]),
-                      Row(children: [
-                        Container(
-                          height: 40,
-                          width: 448,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              width: 1,
                             ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              children: const [
-                                Text("Barcelona Feb 13, 2021",
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w500,
-                                    )),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ])
-                    ]))));
+                          ]),
+                        ]))
+              ],
+            )));
   }
 }
